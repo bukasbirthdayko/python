0e794717b9eeb125d21798e1fd35ae9a52eafec6
EC2 Default User <ec2-user@ip-172-31-41-148.eu-west-1.compute.internal>
2015-07-22 07:43:04 +0000

    revised sanity check

--- stest.py
+++ stest.py
@@ -8,22 +8,37 @@ import datetime as dt
 import json
 
 #token = str(sys.argv[1])
-token = 'NDRhY2U3ZjYtNDY1OC00N2QzLWFkNzEtM2E5NGQ1NmM3YTUx'
-url = 'api-stg1.us-east-1.everywhere.avid.com'
+token = 'YmUyMjBkYzktMDM3OC00ZWYxLTljOTUtZGY5MzYzYzU4ZDk3'
+stgurl = 'api-stg1.us-east-1.everywhere.avid.com'
+
+
+#RENEW TOKEN
+############
+url = 'https://%s/services/avid.iam/tokens/current'
 headers={"Content-Type": "application/json",
         "Authorization" : "Bearer (%s)" % (token) }
-r = requests.patch("https://api-stg1.us-east-1.everywhere.avid.com/services/avid.iam/tokens/current",headers=headers)
-print r.status_code
-rdate = r.content.split(',')
-print rdate[0], rdate[7]
-
+r = requests.patch(url=str(url) % (stgurl),headers=headers)
+#print r.status_code
+resp = json.loads(r.content)
+identity = resp["entity"]["identityId"]
+print identity
+print "\n"
+#CREATE ASSET
+#############
+url = 'https://%s/services/avid.asset/assets'
 data = {"alias" : "$date",
 	"kind" : "asset" }
 data = json.dumps(data)
-r = requests.post("https://api-stg1.us-east-1.everywhere.avid.com/services/avid.asset/assets",data=data,headers=headers)
-print r.status_code
-assetId = r.content.split('"assetId":"')
-assetId = assetId[1]
-assetId = assetId.split('","versionId"')
-assetId = assetId[0]
+r = requests.post(url=str(url) % (stgurl),data=data,headers=headers)
+#print r.status_code
+resp = json.loads(r.content)
+assetId = resp["assetId"]
 print assetId
+print "\n"
+#DELETE ASSET
+#############
+url = 'https://%s/services/avid.asset/assets/%s'
+r = requests.delete(url=str(url) %(stgurl,assetId),headers=headers)
+#print r.status_code
+print str(r.content)
+print "\n"
