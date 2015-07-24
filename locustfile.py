edb18ca3a45db413ae145e392035a1346b276d68
EC2 Default User <ec2-user@ip-172-31-41-148.eu-west-1.compute.internal>
2015-07-24 07:00:14 +0000

    edited locustfile

--- locustfile.py
+++ locustfile.py
@@ -5,30 +5,26 @@ from locust import HttpLocust, TaskSet, task
 import json
 import requests
 
-token = '{TOKEN}'
+token = 'MWM3NDRmNjAtYWJjZS00YzllLTg2NWItY2VhN2Q2ZjA1ODNh'
 stgurl = 'api-stg1.us-east-1.everywhere.avid.com'
 
 class MyTaskSet(TaskSet):
-    @task(2)
+    @task(1)
     def task1(self):
 	headers={"Content-Type": "application/json",
         "Authorization" : "Bearer (%s)" % (token) }
-        self.client.patch("/services/avid.iam/tokens/current",headers=headers)
-
-    @task(1)
-    def task2(self):
-        headers={"Content-Type": "application/json",
-        "Authorization" : "Bearer (%s)" % (token) }
+        r = self.client.patch("/services/avid.iam/tokens/current",headers=headers)
 	url = 'https://%s/services/avid.asset/assets'
 	data = {"alias" : "$date",
         	"kind" : "asset" }
 	data = json.dumps(data)
-	r = requests.post(url=str(url) % (stgurl),data=data,headers=headers)
+	r = self.client.post(url=str(url) % (stgurl),data=data,headers=headers)
 	resp = json.loads(r.content.decode())
 	assetId = resp["assetId"]
+	print assetId
 	url = 'https://%s/services/avid.asset/assets/%s'
-	r = requests.delete(url=str(url) %(stgurl,assetId),headers=headers)
-
+	r = self.client.delete(url=str(url) %(stgurl,assetId),headers=headers)
+	print r
 
 class MyLocust(HttpLocust):
     task_set = MyTaskSet
